1cfb6b24169d575d4217eaa67fc01614b86395a4
Matgorn <m.gorny@selleo.com>
2021-12-02 12:23:33 +0100

    fix: set correct server path

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/events';
+const url = 'http://localhost:8000/events';
 
 export const createEvent = (newEvent) => axios.post(url, newEvent);
 export const fetchEvents = () => axios.get(url);
