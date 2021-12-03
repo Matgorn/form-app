e1a3db12ac6a2f3a03fadd1cfcabb47e5678f9ac
Matgorn <m.gorny@selleo.com>
2021-12-03 17:24:19 +0100

    refactor: remove react query implementation for crud operations

---
+++ client/src/screens/Home/Home.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useCallback } from 'react';
+import { Container, Button } from '@material-ui/core';
+
+import { Events, Form, Modal } from '../../components';
+
+import { onSubmit } from '../../utils';
+
+const Home = () => {
+  const [updateEvents, setUpdateEvents] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleButtonClick = useCallback(() => {
+    setModalOpen((prevState) => !prevState);
+  });
+
+  const handleModalClose = useCallback(() => setModalOpen(false));
+
+  return (
+    <Container maxWidth="sm">
+      <Modal isOpen={modalOpen} handleModalClose={handleModalClose}>
+        <Form
+          setUpdateEvents={setUpdateEvents}
+          onSubmit={onSubmit}
+          handleModalClose={handleModalClose}
+        />
+      </Modal>
+      <Button variant="contained" color="primary" onClick={handleButtonClick}>
+        Add Event
+      </Button>
+      <Events updateEvents={updateEvents} setUpdateEvents={setUpdateEvents} />
+    </Container>
+  );
+};
+
+export default Home;
