63042f6075440d2e04a96290266c85d551bf6c26
goro <m.gorny@selleo.com>
2021-12-06 19:07:17 +0100

    feat: add go back button for event edit screen

--- client/src/screens/EditEvent/EditEvent.jsx
+++ client/src/screens/EditEvent/EditEvent.jsx
@@ -1,14 +1,20 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { getEventById } from '../../store/event/actions';
 import Form from '../../components/Form';
 import { getEventWithValidDate } from '../../store/event/reducer';
+import { Button, Container } from '@material-ui/core';
 
 const EditEvent = () => {
   const { eventId } = useParams();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const { data: event, isLoading, error } = useSelector(getEventWithValidDate, shallowEqual);
+
+  const handleGoBackButtonClick = useCallback(() => {
+    navigate(-1);
+  });
 
   useEffect(() => {
     dispatch(getEventById(eventId));
@@ -19,7 +25,14 @@ const EditEvent = () => {
   } else if (isLoading) {
     return <div>Loading...</div>;
   } else {
-    return <Form initialValues={event} eventId={event._id} />;
+    return (
+      <Container maxWidth="sm">
+        <Form initialValues={event} eventId={event._id} />
+        <Button variant="outlined" onClick={handleGoBackButtonClick}>
+          Go Back
+        </Button>
+      </Container>
+    );
   }
 };
 
