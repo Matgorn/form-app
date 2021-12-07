c81488a338023ec078f6548967e80d5de47efe9f
goro <m.gorny@selleo.com>
2021-12-07 14:18:27 +0100

    feat: add classnames kata

--- client/src/screens/Playground/Playground.jsx
+++ client/src/screens/Playground/Playground.jsx
@@ -1,12 +1,30 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useTitle } from 'react-use';
 import { Field, Form, Formik } from 'formik';
+import classNames from 'classnames';
+
+import './playground.sass';
 
 const Playground = ({ onSubmit }) => {
   useTitle("Let's play");
+  const [blue, setBlue] = useState(false);
+  const [bold, setBold] = useState(false);
+  const [cursive, setCursive] = useState(false);
   const handleFormSubmit = useCallback((values) => () => {
     console.log(values);
     onSubmit(values);
+  });
+
+  const handleColorButtonClick = useCallback(() => {
+    setBlue((prev) => !prev);
+  });
+
+  const handleBoldButtonClick = useCallback(() => {
+    setBold((prev) => !prev);
+  });
+
+  const handleCursiveButtonClick = useCallback(() => {
+    setCursive((prev) => !prev);
   });
 
   return (
@@ -34,6 +52,19 @@ const Playground = ({ onSubmit }) => {
           </Form>
         )}
       </Formik>
+
+      <p
+        className={classNames({
+          btn: true,
+          blue: blue,
+          bold: bold,
+          cursive: cursive
+        })}>
+        Classnames presentation
+      </p>
+      <button onClick={handleColorButtonClick}>Blue: {blue ? 'true' : 'false'}</button>
+      <button onClick={handleBoldButtonClick}>Bold: {bold ? 'true' : 'false'}</button>
+      <button onClick={handleCursiveButtonClick}>Cursive: {cursive ? 'true' : 'false'}</button>
     </div>
   );
 };
